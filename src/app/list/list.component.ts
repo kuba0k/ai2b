bf73675b90d22f099a3b427fdcb535b0aca44159
Jakub <kubaok96@gmail.com>
2023-12-02 01:17:11 +0100

    Add list and details components

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {Person} from "../person";
+import {PersonLsService} from "../person-ls.service";
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit{
+  people: Person[] = [];
+  constructor(
+    private personLsService: PersonLsService,
+  ) {
+  }
+  ngOnInit():void{
+    this.people = this.personLsService.getAll();
+  }
+}
